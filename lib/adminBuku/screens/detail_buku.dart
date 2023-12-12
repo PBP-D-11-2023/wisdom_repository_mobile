e8a40893a3696a842939cbef4111537b6236a968
RobertBenyamin <robertbenyamino@gmail.com>
2023-12-12 21:35:00 +0700

    fix: UI for detail book page

--- lib/adminBuku/screens/detail_buku.dart
+++ lib/adminBuku/screens/detail_buku.dart
@@ -18,63 +18,198 @@ class DetailBuku extends StatelessWidget {
     final request = context.watch<CookieRequest>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(buku.fields.judul),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(buku.fields.gambar),
-            Text(buku.fields.judul),
-            Text(buku.fields.penulis),
-            Text(buku.fields.deskripsi),
-            Text(buku.fields.kategori),
-            Text(buku.fields.tahun.toString()),
-            Text(buku.fields.rating.toString()),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => EditFormPage(
-                                  buku: buku,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image.asset(
+                    'assets/images/banner.png',
+                    height: 250,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Center(
+                        child: Container(
+                      padding: EdgeInsets.all(24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            color: Colors.white,
+                            height: 175,
+                            width: 125,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.network(buku.fields.gambar),
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  buku.fields.judul,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  buku.fields.penulis,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  '${buku.fields.rating.toString()}/5',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  buku.fields.tahun.toString(),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  buku.fields.kategori,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Details",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: double.infinity, // Set the width to full
+                      height: 400,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xFF37465D), width: 2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            buku.fields.deskripsi,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                            ),
+                            backgroundColor: Color(0xFF37465D),
+                          ),
+                          child: const Text(
+                            'Back',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EditFormPage(
+                                      buku: buku,
+                                    ),
+                                  ),
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0),
+                                ),
+                                backgroundColor: Color(0xFF4DC7BF),
+                              ),
+                              child: const Text(
+                                'Edit',
+                                style: TextStyle(
+                                  color: Color(0xFF37465D),
                                 ),
                               ),
-                            );
-              },
-              child: const Text('Edit'),
-            ),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await request.postJson(
-                            "https://wisdomrepository--wahyuridho5.repl.co/admin-buku/delete-book-flutter/",
-                            jsonEncode(<String, String>{
-                              'bookID': buku.pk.toString(),
-                            }));
-                if (response['status'] == 'success') {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(const SnackBar(
-                    content: Text("Buku berhasil dihapus!"),
-                  ));
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AdminPage()),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(const SnackBar(
-                    content:
-                        Text("Terdapat kesalahan, silakan coba lagi."),
-                  ));
-                }
-              },
-              child: const Text('Hapus'),
-            ),
-            const SizedBox(height: 8),
-          ],
+                            ),
+                            SizedBox(width: 8),
+                            ElevatedButton(
+                              onPressed: () async {
+                                final response = await request.postJson(
+                                    "https://wisdomrepository--wahyuridho5.repl.co/admin-buku/delete-book-flutter/",
+                                    jsonEncode(<String, String>{
+                                      'bookID': buku.pk.toString(),
+                                    }));
+                                if (response['status'] == 'success') {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text("Buku berhasil dihapus!"),
+                                  ));
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const AdminPage()),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text(
+                                        "Terdapat kesalahan, silakan coba lagi."),
+                                  ));
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0),
+                                ),
+                                backgroundColor: Color(0xFFFC726F),
+                              ),
+                              child: const Text(
+                                'Delete',
+                                style: TextStyle(
+                                  color: Color(0xFF37465D),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
